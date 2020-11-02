4a6aef38c80960c9b2399b86521decae5d4cf93e
Anup Lamsal <anuplamsal7410@gmail.com>
2020-11-02 20:36:12 +0545

    Updates

---
+++ precache-manifest.99d9ae1d98618da4799d64f7d23a4b5a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ff6d807a78d87c8422648db6b807fe",
+    "url": "/React-Web/index.html"
+  },
+  {
+    "revision": "3fc1105691852cd46982",
+    "url": "/React-Web/static/css/main.7a6162e1.chunk.css"
+  },
+  {
+    "revision": "afaa652e1cfa2e6137e2",
+    "url": "/React-Web/static/js/2.5a2f7b2d.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/React-Web/static/js/2.5a2f7b2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fc1105691852cd46982",
+    "url": "/React-Web/static/js/main.17a78e6c.chunk.js"
+  },
+  {
+    "revision": "4c3a4a9cf73d1a3a9119",
+    "url": "/React-Web/static/js/runtime-main.7eaf9f64.js"
+  },
+  {
+    "revision": "ac5a83b7f6b642868d2a3c8124d22464",
+    "url": "/React-Web/static/media/img1.ac5a83b7.jpeg"
+  },
+  {
+    "revision": "fe3c12d083f1db3701523a24c07e8312",
+    "url": "/React-Web/static/media/img2.fe3c12d0.jpeg"
+  },
+  {
+    "revision": "1e60b05578dc32cdf3e7a277ee9c9997",
+    "url": "/React-Web/static/media/img3.1e60b055.jpeg"
+  },
+  {
+    "revision": "46ef256d2f2775b365db8c4bf4764baf",
+    "url": "/React-Web/static/media/img4.46ef256d.jpeg"
+  }
+]);
